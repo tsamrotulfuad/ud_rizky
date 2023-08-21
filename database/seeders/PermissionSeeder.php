ffce45a143dea196ada7c9cbeb8129e8601d320d
Tsamrotul Fuad <tsamrotulfuad@live.com>
2023-08-22 06:27:37 +0700

    Add Permission

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PermissionSeeder extends Seeder
 {
@@ -12,6 +14,26 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //create permissions kategori
+        Permission::create(['name' => 'view kategori']);
+        Permission::create(['name' => 'create kategori']);
+        Permission::create(['name' => 'edit kategori']);
+        Permission::create(['name' => 'delete kategori']);
+        
+        //create permissions supplier
+        Permission::create(['name' => 'view supplier']);
+        Permission::create(['name' => 'create supplier']);
+        Permission::create(['name' => 'edit supplier']);
+        Permission::create(['name' => 'delete supplier']);
+
+        //create permissions produk
+        Permission::create(['name' => 'view produk']);
+        Permission::create(['name' => 'create produk']);
+        Permission::create(['name' => 'edit produk']);
+        Permission::create(['name' => 'delete produk']);
+
+        //create roles and assign existing permissions
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo('');
     }
 }
