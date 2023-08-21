2a9d1ecee7e7d97c142b57824b42771f272fdd82
Tsamrotul Fuad <tsamrotulfuad@live.com>
2023-08-21 17:17:07 +0700

    layout kasir

--- resources/views/kasir/order.blade.php
+++ resources/views/kasir/order.blade.php
@@ -6,55 +6,43 @@
             <div class="col-sm-12 mb-3">
                 <div class="card" style="width: auto;">
                     <div class="card-body">
-                        <div class="d-flex">
-                            <div class="row">
-                                <div class="col-md-auto">
-                                    <input type="text" class="form-control g-3" id="exampleFormControlInput1"
-                                        placeholder="Scan Barcode">
-                                </div>
-                                <div class="col">
-                                    <button type="button" class="btn btn-primary">Pilih barang</button>
-                                </div>
+                        <div class="row row-cols-2">
+                            <div class="col">
+                                <input type="text" class="form-control" name="scanBarcode" id="scanBarcode"
+                                    aria-describedby="scanBarcode">
+                            </div>
+                            <div class="col">
+                                <button type="button" class="btn btn-primary">Primary</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-sm-12">
-                <div class="card" style="width: auto;">
-                    <div class="card-body">
-                        <table class="table table-borderless caption-top">
-                            <caption>Daftar Barang</caption>
-                            <thead>
-                                <tr>
-                                    <th scope="col" style="width: 5px;">No</th>
-                                    <th scope="col">Item</th>
-                                    <th scope="col">Harga</th>
-                                    <th scope="col">Qty</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         <div class="col-md-5 col-lg-4 order-md-last">
@@ -97,7 +85,8 @@
 
             <form class="card p-2">
                 <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Promo code">
+                    <input type="text" class="form-control" id="nominalBayar" name="nominalBayar"
+                        placeholder="Masukkan Nominal">
                     <button type="submit" class="btn btn-secondary">Bayar</button>
                 </div>
             </form>
